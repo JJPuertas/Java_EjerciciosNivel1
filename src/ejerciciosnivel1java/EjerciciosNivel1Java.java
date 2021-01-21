f04141a93448bfd86448e93f96a0eba3fd8286b0
jpb20 <jpb20@LAPTOP-ACVN5DIU>
2021-01-21 18:52:45 +0100

    EJERCICIO 1 TERMINADO QUEDAN 2.

--- src/ejerciciosnivel1java/EjerciciosNivel1Java.java
+++ src/ejerciciosnivel1java/EjerciciosNivel1Java.java
@@ -5,17 +5,46 @@
  */
 package ejerciciosnivel1java;
 
+import java.util.Arrays;
+
 /**
  *
  * @author jpb20
  */
 public class EjerciciosNivel1Java {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    // declaro el array de ints para hacer pruebas 
+    int[] listaNumeros = {3000, 600, 70000, 200, 2000000, 955519};
+
+    public int[] maximo(int[] lista) {
+
+        int[] sol = {0, 0};  // Creo un vector vacio para almacenar la solucion
+        int aux = lista[0];  // El máximo va a ser el primer elemento del array
+        int aux2 = lista[0]; // Creo el segundo auxiliar para empezar a rastrear
+        int j;
+
+        for (j = 1; j < lista.length; j++) {
+            if (aux < lista[j]) {
+                aux = lista[j];
+            }
+            sol[0] = aux;
+        }
+
+        for (int i = 1; i < lista.length; i++) {
+            if (aux2 < lista[i] && lista[i] != aux) {
+                aux2 = lista[i];
+            }
+
+            sol[1] = aux2;
+        }
+
+        return sol;
     }
-    
+
+    public static void main(String[] args) {
+        EjerciciosNivel1Java ejercicio = new EjerciciosNivel1Java();
+
+        System.out.println("Los valores maximos son: " + Arrays.toString(ejercicio.maximo(ejercicio.listaNumeros)));
+    }
+
 }
